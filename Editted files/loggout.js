116fbea348ff628bac5540209dc65c9be4d05a5c
Alrkhayes <131044672+Alrkhayes@users.noreply.github.com>
2024-12-17 02:56:29 -0700

    Delete Editted files directory

--- Editted files/loggout.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('logout').addEventListener('click', () => {
-    localStorage.removeItem('userId');
-    localStorage.removeItem('email');
-    window.location.href = '/login.html';
-});
